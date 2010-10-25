97123820a55840d4644f6a0e663f9e2e7949ca2c
Eugene Ma <Eugene Ma@98.207.63.92>
2010-10-25 15:45:02 -0700

    Moved Victor's helper functions code for semi-supervised domain into the file containing the other helper functions for the domain.
    Signed-off-by: Eugene Ma <Eugene Ma@98.207.63.92>

--- src/domains/semiSupervisedLearning/RunForSemiSupervisedLearningDomain.java
+++ src/domains/semiSupervisedLearning/RunForSemiSupervisedLearningDomain.java
@@ -245,10 +245,35 @@ public class RunForSemiSupervisedLearningDomain {
 					+ "and the outDatashardPath should be the third argument.");
 		}
 		else {
-			HelperFunctions.stripLabels(args[1], args[2]);
+			stripLabels(args[1], args[2]);
 		}
 	}
 
+	/**
+	 * 
+	 * @author Victor
+	 */
+	protected static void stripLabels(String inDataShardPath, String outDataShardPath) {
+		try {
+			BufferedReader inDataShard = new BufferedReader(new FileReader(
+					inDataShardPath));
+			BufferedWriter outDataShard = new BufferedWriter(new FileWriter(
+					outDataShardPath));
+			try {
+				String line = null;
+				while ((line = inDataShard.readLine()) != null) {
+					outDataShard.write("u" + line.substring(line.indexOf(" "))
+							+ "\n");
+				}
+			} finally {
+				inDataShard.close();
+				outDataShard.close();
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
 	// *******************************************************************************
 
     protected static void evaluate(String[] args) {
@@ -257,7 +282,52 @@ public class RunForSemiSupervisedLearningDomain {
 					+ "and the predictionDatashardPath should be the third argument.");
 		}
 		else {
-			HelperFunctions.evaluate(args[1], args[2]);
+			evaluate(args[1], args[2]);
 		}
     }
+    
+    /**
+	 * 
+	 * @author Victor
+	 */
+    protected static void evaluate(String dataShardPath, String predictionPath) {
+		try {
+			BufferedReader dataShard = new BufferedReader(new FileReader(dataShardPath));
+			BufferedReader predictions = new BufferedReader(new FileReader(predictionPath));
+			BufferedWriter status = new BufferedWriter(new FileWriter("status"));
+			try {
+				String line = null;
+				List<String> correctLabels = new ArrayList<String>();
+				List<String> predictedLabels = new ArrayList<String>();
+				while ((line = dataShard.readLine()) != null) {
+					correctLabels.add(line);
+				}
+				while ((line = predictions.readLine()) != null) {
+					predictedLabels.add(line);
+				}
+				if (correctLabels.size() == predictedLabels.size()) {
+					double correct = 0.0;
+					double incorrect = 0.0;
+					double total = 0.0;
+					for (int i = 0; i < correctLabels.size(); i++) {
+						if (correctLabels.get(i).equals(predictedLabels.get(i))) {
+							correct++;
+						} else {
+							incorrect++;
+						}
+						total++;
+					}
+					status.write("numErrors: " + incorrect + "\n");
+					status.write("numExamples: " + total + "\n");
+					status.write("errorRate: " + incorrect / total + "\n");
+				}
+			} finally {
+				dataShard.close();
+				predictions.close();
+				status.close();
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
